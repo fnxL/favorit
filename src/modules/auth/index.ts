3553aafe1e5e8fc34adfa142824937cff478747b
Ajay Kanki <48607132+fnxL@users.noreply.github.com>
2024-06-12 23:34:29 +0530

    chore: create index file for auth module

---
+++ src/modules/auth/index.ts
@@ -0,0 +1,6 @@
+import AuthService from './auth.service';
+import AuthRepository from './auth.repository';
+
+export * from './auth.dto';
+export * from './auth.model';
+export { AuthService, AuthRepository };
